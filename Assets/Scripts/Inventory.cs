34deb6de5e768f14e94e38d7b4ce3c9d679df695
SurDno <surdno@yandex.ru>
2024-11-07 16:03:15 +0000

    elie is a great guy

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+    public List<SpriteRenderer> sprites;
+    public List<Item> items;
+
+    public void AddItem(Item item) {
+        
+    }
+}
