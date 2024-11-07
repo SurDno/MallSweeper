480db37d157130b0338921c6b01b7d863e51f415
SurDno <surdno@yandex.ru>
2024-11-07 16:12:43 +0000

    inventory (need to edit scene)

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Inventory : MonoBehaviour {
-    public List<SpriteRenderer> sprites;
+public class Inventory : Singleton<Inventory> {
+    public List<Image> sprites;
     public List<Item> items;
-
+    public int index;
+    
     public void AddItem(Item item) {
+        if (index > 5)
+            return;
         
+        items.Add(item);
+        sprites[index].sprite = items[index].Image;
+        sprites[index].enabled = true;
+        index++;
     }
 }
