90b6b6faf1ace9182f3a0eb395728e4390225f8f
SurDno <surdno@yandex.ru>
2024-11-08 15:01:37 +0000

    fixed doors maybe

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,12 +5,12 @@ public class Door : MonoBehaviour
     public Transform teleportLocation; // Assign this in the inspector for each door's specific teleport spot
     private Transform playerTransform;
     private bool playerInRange = false;
-    public SpriteRenderer sprite;
+    public SpriteRenderer sprite, spriteE;
 
     void Start()
     {
         playerTransform = PlayerControl.Instance.transform;
-        sprite.enabled = false;
+        spriteE.enabled = false;
     }
 
     void Update()
@@ -18,8 +18,8 @@ public class Door : MonoBehaviour
         playerInRange = Vector2.Distance(playerTransform.position, transform.position) < 1f;
         var distance123 = Vector2.Distance(playerTransform.position, transform.position);
         // Check if the player presses 'F' while in range of the door
-        sprite.enabled = playerInRange;
-        if (playerInRange && Input.GetKeyDown(KeyCode.F))
+        spriteE.enabled = playerInRange;
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
         {
             if (teleportLocation != null)
             {
